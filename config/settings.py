827549748456e144239124a6db56999eb001c7ef
Sarah Darlington <sazzzel@gmail.com>
2024-11-27 12:46:15 +0000

    Add: Admin & User stories to readme, changed debug back to if statment

--- config/settings.py
+++ config/settings.py
@@ -15,9 +15,9 @@ import os
 import dj_database_url
 if os.path.isfile("env.py"):
     import env
-#     DEBUG = True
-# else:
-#     DEBUG = False
+    DEBUG = True
+else:
+    DEBUG = False
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,7 +31,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-sazzzel-blogggel-223k64jkm88.ws.codeinstitute-ide.net', 
