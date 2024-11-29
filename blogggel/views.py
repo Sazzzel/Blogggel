d44274b7a18e77540c33ed39d45d8435004c5933
Sarah Darlington <sazzzel@gmail.com>
2024-11-29 15:28:28 +0000

    Add: pinstalled allauth and added postlist to views

--- blogggel/views.py
+++ blogggel/views.py
@@ -1,10 +1,61 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.shortcuts import render, get_object_or_404, reverse
+from django.views import generic
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from .models import Post, Comment
+from .forms import CommentForm
 
 # Create your views here.
 
-class HomePage(TemplateView):
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1)
+    template_name = "blog/index.html"
+    paginate_by = 6
+
+def post_detail(request, slug):
+
     """
-    Displays home page"
+    Display an individual :model:`blog.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+
+    **Template:**
+
+    :template:`blog/post_detail.html`
     """
-    template_name = 'base.html'
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    comments = post.comments.all().order_by("-created_on")
+    comment_count = post.comments.filter(approved=True).count()
+    
+
+    if request.method == "POST":
+        print("Received a POST request")
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+            )
+    
+    comment_form = CommentForm()
+    print("About to render template")
+
+    return render(
+        request,
+        "blog/post_detail.html",
+        {
+            "post": post,
+            "comments": comments,
+            "comment_count": comment_count,
+            "comment_form": comment_form
+        },
+    )
