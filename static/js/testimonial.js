098844d37a0efaa7981faf1b62819ed068f1712b
Sarah Darlington <sazzzel@gmail.com>
2024-12-04 18:32:36 +0000

    Add: edit testimonials now opens modal and shows text

--- static/js/testimonial.js
+++ static/js/testimonial.js
@@ -1,9 +1,11 @@
 const editButtons = document.getElementsByClassName("btn-testimonial-edit");
-const testimonialText = document.getElementById("id_body");
-const testimonialForm = document.getElementById("commentForm");
+const testimonialText = document.getElementById("id_text");
+const testimonialJob = document.getElementById("id_job_title")
+const testimonialForm = document.getElementById("testimonialForm");
 const submitButton = document.getElementById("submitButton");
 
 const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
+const addModal = new bootstrap.Modal(document.getElementById("addTestimonialModal"))
 const deleteButtons = document.getElementsByClassName("btn-testimonial-delete");
 const deleteConfirm = document.getElementById("deleteConfirm");
 
@@ -30,8 +32,13 @@ for (let button of deleteButtons) {
 
   for (let button of editButtons) {
     button.addEventListener("click", (e) => {
-      let testimonialId = e.target.getAttribute("testimonial_id");
-      deleteConfirm.href = `delete_testimonial/${testimonialId}`;
-      deleteModal.show();
+      const testimonialId = e.target.getAttribute("testimonial_id");
+      const content = document.getElementById(`testimonial-text${testimonialId}`).innerText;
+      const job = document.getElementById(`testimonial-job${testimonialId}`).innerText;
+      testimonialText.value = content;
+      testimonialJob.value = job;
+      submitButton.innerText = "Update";
+      //testimononialForm.setAttribute("action", `edit_testimonial/${testimonialId}`);
+      addModal.show();
     });
   }
