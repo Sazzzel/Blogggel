83830fb5c4adf244793634cdf2a98c48655100f5
Sarah Darlington <sazzzel@gmail.com>
2024-12-09 20:49:03 +0000

    Edit crispy forms to not show model names and add a bit of style to the UI.

--- blogggel/forms.py
+++ blogggel/forms.py
@@ -6,10 +6,19 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Modify existing field attributes
+        self.fields['body'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Enter your comment'})
+        self.fields['body'].label = "Comment:"  # Set the label
 
 
 class TestimonialForm(forms.ModelForm):
     class Meta:
         model = Testimonial
         fields = ('job_title', 'text', 'id')
- 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Modify existing field attributes
+        self.fields['job_title'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Your role...'})
+        self.fields['text'].label = "Comment:"  # Set the label
